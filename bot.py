b7c14a79da61c1271fd8c13325f938fb056d500d
Masahme <115889084+Masahme@users.noreply.github.com>
2023-03-12 14:29:26 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -17,20 +17,14 @@ if __name__ == "__main__":
     plugins = dict(root="chatgpt")
 
 app = Client(
-
-    "my_bot",
-
+    name="app",
     api_id=API_ID,
-
     api_hash=API_HASH,
-
     bot_token=BOT_TOKEN,
-
     plugins=plugins,
-
     workers=300,
-
 )
+
 
 app.run()
 
